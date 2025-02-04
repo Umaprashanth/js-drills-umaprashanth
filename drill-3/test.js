ee9331c25b4a7558824fa8ac0254e7603b04b0f5
Umaprashanth <umaprashanthj@gmail.com>
2025-02-04 15:21:52 +0530

    adding drill -3 files

---
+++ drill-3/test.js
@@ -0,0 +1,14 @@
+import inventory from "./cars.js";
+import {carInfo} from "./solution.js"
+
+// var id = 53
+// var id = 33
+
+// let car = carInfo(inventory,id);
+//     if (car == false){
+//         console.log("Id is out of range")
+//     } else{
+// console.log(`Car ${id} is a ${car.car_year} ${car.car_make} ${car.car_model}`) 
+//     }
+
+

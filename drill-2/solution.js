38b83f36860909eec2a12a672e70bb61f3cc8e51
Umaprashanth <umaprashanthj@gmail.com>
2025-02-04 14:28:08 +0530

    adding drill-2 files

---
+++ drill-2/solution.js
@@ -0,0 +1,92 @@
+import users from "./users.js";
+
+// Q1 Find all users who are interested in playing video games.
+
+function usersInterestedInVideoGames(data) {
+  var usersList = [];
+  if (!data){
+    // console.log("Please provide the data")
+    return "Please provide the data"
+    }
+
+  for (let obj in data) {
+    if (
+        typeof data[obj] === "object" && data[obj].interests && Array.isArray(data[obj].interests)
+    ){
+    if (data[obj]["interests"][0].includes("Video Games")) {
+      usersList.push(obj);
+    }
+  }
+}
+  return usersList;
+}
+
+// Q2 Find all users staying in Germany.
+
+function usersInGermany(data) {
+
+    if (!data){
+        return "Please provide the data"
+        }
+  let usersList = [];
+
+  for (let obj in data) {
+    if (
+        typeof data[obj] === "object" && data[obj].nationality
+    ){
+    if (data[obj]["nationality"].includes("Germany")) {
+      usersList.push(obj);
+    }
+  }
+}
+  return usersList;
+}
+
+// Q3 Find all users with masters Degree.
+
+function usersWithMasters(data) {
+    if (!data){
+        return "Please provide the data"
+        }
+  let usersList = [];
+
+  for (let obj in data) {
+    if (
+        typeof data[obj] === "object" && data[obj].qualification
+    ){
+    if (data[obj]["qualification"].includes("Masters")) {
+      usersList.push(obj);
+    }
+}
+  }
+  return usersList;
+}
+
+// Q4 Group users based on their Programming language mentioned in their designation.
+
+function designationBasedOnLanguage(data) {
+    if (!data){
+
+        return "Please provide the data"
+        }
+  const language = { python: [], javascript: [], golang: [] };
+
+  for (let obj in data) {
+    if (typeof data[obj] === "object" && data[obj].designation){
+    for (let lang in language) {
+      if (data[obj].designation.toLowerCase().includes(lang)) {
+        language[lang].push(obj);
+      }
+    }
+    }
+  }
+  return language;
+}
+
+export {
+    usersInterestedInVideoGames,
+    usersInGermany,
+    usersWithMasters,
+    designationBasedOnLanguage
+
+}
---
+++ drill-2/test.js
@@ -0,0 +1,37 @@
+import users from "./users.js"
+import {
+    usersInterestedInVideoGames,
+    usersInGermany,
+    usersWithMasters,
+    designationBasedOnLanguage
+} from "./solution.js"
+
+var newData = []
+
+// console.log(usersInterestedInVideoGames(users)); //[ 'John', 'Ron' ]
+// console.log(usersInterestedInVideoGames()); //Please provide the data
+// console.log(usersInterestedInVideoGames(newData)); //[]
+
+// console.log(usersInGermany(users)); //[ 'Wanda', 'Pike' ]
+// console.log(usersInGermany()); //Please provide the data
+// console.log(usersInGermany(newData)); //[]
+// console.log(usersInGermany("hello")); //[]
+
+// console.log(usersWithMasters(users)); //[ 'John', 'Rob' ]
+// console.log(usersWithMasters()); //Please provide the data
+// console.log(usersWithMasters("hello")); //[]
+// console.log(usersWithMasters(newData)); //[]
+
+
+// console.log(designationBasedOnLanguage(users));
+// {
+// python: [ 'Pike' ],
+// javascript: [ 'Wanda', 'Rob' ],
+// golang: [ 'John', 'Ron' ]
+// }
+// console.log(designationBasedOnLanguage()); //Please provide the data
+// console.log(designationBasedOnLanguage(newData)); //{ python: [], javascript: [], golang: [] }
+// console.log(designationBasedOnLanguage("hello")); //{ python: [], javascript: [], golang: [] }
+
+
+
